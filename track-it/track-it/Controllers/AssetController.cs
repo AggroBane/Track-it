df5ed92bdade8368646bdaa56bf11f60e264a44e
Psycho <56837234+psychopattt@users.noreply.github.com>
2021-10-29 14:34:31 -0400

    Asset controller + android studio gitignore

---
+++ track-it/track-it/Controllers/AssetController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using track_it.Entities;
+
+namespace track_it.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AssetController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/asset")]
+        public Asset Get()
+        {
+            return new Asset();
+        }
+    }
+}
