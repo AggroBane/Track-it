0db26b2bb6a014d5b512a1a9fce62dbeac365a5a
Jordan <itsseraphii@gmail.com>
2021-10-29 15:13:15 -0400

    There is now a ponit on the map :smile:

--- track-it/track-it/ClientApp/src/components/Map.vue
+++ track-it/track-it/ClientApp/src/components/Map.vue
@@ -1,14 +1,26 @@
 <template>
   <GoogleMap
-      :center="{lat: 40.689247, lng: -74.044502}"
+      :api-key="apiKey"
+      :center="somePoint"
       :zoom="15"
       style="width: 100%; height: 500px">
+    <Marker :options="{ position: somePoint }"/>
   </GoogleMap>
 </template>
 <script>
-import {GoogleMap} from "vue3-google-map";
+import {GoogleMap, Marker} from "vue3-google-map";
 
 export default {
-  name: "Maps", components: {GoogleMap}
+  name: "Maps", components: {GoogleMap, Marker},
+  data() {
+    return {
+      somePoint: {lat: 40.689247, lng: -74.044502}
+    }
+  },
+  computed: {
+    apiKey() {
+      return process.env.VUE_APP_GOOGLE_API_KEY;
+    }
+  }
 }
 </script>
