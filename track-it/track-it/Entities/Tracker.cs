7a9f7b5930bd3cf8d280cac78807848f359fd541
William Fecteau <william-fecteau@hotmail.com>
2021-10-29 15:08:40 -0400

    Added in memory db

--- track-it/track-it/Entities/Tracker.cs
+++ track-it/track-it/Entities/Tracker.cs
@@ -4,7 +4,7 @@ namespace track_it.Entities
 {
     public class Tracker
     {
-        public string SerialId { get; set; }
+        public string Id { get; set; }
         public float Lat { get; set; }
         public float Lng { get; set; }
         public DateTime LastPingUtc { get; set; }
