aa74c84cb670fdeb3ad0f3a1981857fb5a462203
Jordan <itsseraphii@gmail.com>
2021-10-29 21:29:57 -0400

    Run app on port 80

--- track-it/track-it/ClientApp/src/components/Home.vue
+++ track-it/track-it/ClientApp/src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex flex-column justify-content-center">
+  <div class="d-flex flex-column justify-content-center mt-5">
     <h1>Track-It</h1>
     <img class="my-4 align-self-center" src="../assets/trackitlogo.png" style="height: 16rem; width:16rem;"/>
     <p>An all in one solution for all your tracking needs!</p>
--- track-it/track-it/ClientApp/src/components/Map.vue
+++ track-it/track-it/ClientApp/src/components/Map.vue
@@ -1,47 +1,53 @@
 <template>
   <GoogleMap id="map"
-      :api-key="apiKey"
-      :center="somePoint"
-      :zoom="15"
-      style="width: 100%;"
-      :style="{height: myheight}">
-    <Marker v-for="point in manyPoints" :key="point.lat" :options="{position: point}" @click="scream(point.title)"/>
+             ref="mapRef"
+             :api-key="apiKey"
+             :center="mapCenterPoint"
+             :style="{height: myheight}"
+             :zoom="15"
+             style="width: 100%;">
+    <Marker v-for="point in Object.values(manyPoints)" :key="point.lat" :options="{position: point}"
+            @click="markerClicked(point.name)"/>
   </GoogleMap>
 </template>
 
 <style>
-.gmnoprint{
-    margin-top: 65% !important;
+.gmnoprint {
+  margin-top: 65% !important;
 }
 </style>
-
-
 <script>
 import {GoogleMap, Marker} from "vue3-google-map";
 import $ from 'jquery';
+import {arrayToDictionary} from "../../store/utilities";
 
 export default {
   name: "Maps", components: {GoogleMap, Marker},
   data() {
     return {
+      mapCenterPoint: {lat: 0, lng: 0},
       somePoint: {lat: 46.6120085, lng: -71.1074071},
-      manyPoints: [],
+      manyPoints: {},
     }
   },
   mounted() {
     //TODO poke backend for our points
-    this.manyPoints = [
-      {lat: 46.6120085, lng: -71.1074071, title: "j'ai mal"},
-      {lat: 47.6120085, lng: -70.1074071, title: "j'ai mal2"},
-      {lat: 48.6120085, lng: -72.1074071, title: "j'ai mal3"},
-      {lat: 45.6120085, lng: -73.1074071, title: "j'ai mal5"}
-    ];
+    this.mapCenterPoint = this.somePoint;
+    this.manyPoints = arrayToDictionary([
+      {lat: 46.6120085, lng: -71.1074071, name: "marker1"},
+      {lat: 47.6120085, lng: -70.1074071, name: "marker2"},
+      {lat: 48.6120085, lng: -72.1074071, name: "marker3"},
+      {lat: 45.6120085, lng: -73.1074071, name: "marker4"}
+    ], 'name');
   },
   methods: {
     scream(title) {
       let iCry = "AAAAAAAAAAAAAAAAAAAAAAAA " + title;
       console.log(iCry);
       this.$toast.success(iCry, {position: 'top-right'});
+    },
+    markerClicked(markerName) {
+      this.mapCenterPoint = this.manyPoints[markerName];
     }
   },
   computed: {
